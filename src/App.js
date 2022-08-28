d86ad5416c8cacdb38ca70ffb6a5db8fa1481fa4
hafizbali315 <hafizbali315@gmail.com>
2022-08-29 01:12:37 +0500

    Info Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Layout from './components/Layout'
 import HomePage from './pages/Home'
+import InfoPage from './pages/Info'
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 				<Routes>
 					<Route path="" element={<Layout />}>
 						<Route index element={<HomePage />} />
+						<Route path="/info" element={<InfoPage />} />
 					</Route>
 				</Routes>
 			</Router>
--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,5 +1,59 @@
+import { Link } from 'react-router-dom'
+import { BsInstagram, BsTwitter, BsYoutube, BsVimeo } from 'react-icons/bs'
+import { FaSoundcloud } from 'react-icons/fa'
+import { GoMail } from 'react-icons/go'
+
+import InfoBackgroundVideo from '../assets/InfoBackgroundVideo.mp4'
+
 const InfoPage = () => {
-	return <div className="info_page">InfoPage</div>
+	return (
+		<div className="info_page">
+			<video className="background_video" autoPlay muted loop id="background_video" controls="nodownload">
+				<source src={InfoBackgroundVideo} type="video/mp4" />
+			</video>
+
+			<div className="content">
+				<h1>
+					TRUE STUDIO
+					<br />
+					Via Dei Condotti, 93, 00187 Roma
+				</h1>
+
+				<ul className="social_links">
+					<li>
+						<Link to="https://www.instagram.com/cargoworld/" className="link" target="_blank">
+							<BsInstagram className="icon" />
+						</Link>
+					</li>
+					<li>
+						<Link to="https://twitter.com/cargocollective" className="link" target="_blank">
+							<BsTwitter className="icon" />
+						</Link>
+					</li>
+					<li>
+						<Link to="https://www.youtube.com/" className="link" target="_blank">
+							<BsYoutube className="icon" />
+						</Link>
+					</li>
+					<li>
+						<Link to="https://soundcloud.com/cargocollective" target="_blank" className="link">
+							<FaSoundcloud className="icon" />
+						</Link>
+					</li>
+					<li>
+						<Link to="https://vimeo.com/" target="_blank" className="link">
+							<BsVimeo className="icon" />
+						</Link>
+					</li>
+					<li>
+						<Link to="mailto:theericshawn12@gmail.com" target="_blank" className="link">
+							<GoMail className="icon" />
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</div>
+	)
 }
 
 export default InfoPage
